d27ad335c4da7994169057e40c2dcd215fcf64bd
AleksandrKosyakov <106806983+AleksandrKosyakov@users.noreply.github.com>
2022-06-09 02:34:33 +0500

    Add files via upload

---
+++ Проект 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Проект_2
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Load load = new Load();
+            DateTime end = DateTime.Now + TimeSpan.FromSeconds(5);
+            load.Show();
+            while(end>DateTime.Now)
+            {
+                Application.DoEvents();
+            }
+            load.Close();
+            load.Dispose();
+            Application.Run(new Main());
+        }
+    }
+}
