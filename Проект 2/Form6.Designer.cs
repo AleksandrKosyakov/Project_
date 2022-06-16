086ddbe66648badac7c3f125b832e104dc8cb2ac
AleksandrKosyakov <106806983+AleksandrKosyakov@users.noreply.github.com>
2022-06-16 13:02:25 +0500

    Add files via upload

---
+++ Проект 2/Form6.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Проект_2
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(83, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Linen;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(78, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(407, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Восхождение Гитлера к власти";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Linen;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(83, 485);
+            this.label2.MaximumSize = new System.Drawing.Size(400, 0);
+            this.label2.MinimumSize = new System.Drawing.Size(400, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(400, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Как Адольф Гитлер и нацистская партия привлекали избирателей в начале 1930-х годо" +
+    "в?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(83, 545);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 182);
+            this.textBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Linen;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(507, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Linen;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 48);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_back
+            // 
+            this.btn_back.AutoSize = true;
+            this.btn_back.BackColor = System.Drawing.Color.Linen;
+            this.btn_back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_back.Location = new System.Drawing.Point(507, 71);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(51, 48);
+            this.btn_back.TabIndex = 28;
+            this.btn_back.Text = "Назад";
+            this.btn_back.UseVisualStyleBackColor = false;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.Linen;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(12, 71);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 48);
+            this.button4.TabIndex = 41;
+            this.button4.Text = "Пауза";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Linen;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(249, 734);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Linen;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(297, 734);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 20);
+            this.label4.TabIndex = 43;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Linen;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(275, 734);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 20);
+            this.label5.TabIndex = 44;
+            this.label5.Text = ":";
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Linen;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(12, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 48);
+            this.button2.TabIndex = 45;
+            this.button2.Text = "Проп.";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(569, 783);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form6";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Восхождение Гитлера к власти";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Проект 2/Load.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace Проект_2
+{
+    public partial class Load : Form
+    {
+        Font myFont1;
+        public Load()
+        {
+            InitializeComponent();
+            LoadFont();
+            //Присваивание шрифта элементам
+            label1.Font = myFont1;
+            //Запуск таймра, по истечению которого форма закрывается и открывается главное меню
+            timer1.Start();
+            text();
+        }
+        private void LoadFont()
+        {
+            // Создание нового шрифта.
+            PrivateFontCollection custom_font = new PrivateFontCollection();
+            custom_font.AddFontFile("Ironmonger Black Regular.otf");
+            myFont1 = new Font(custom_font.Families[0], 25);
+        }
+        private void text()
+        {
+            //Метод, в котором выбирается случайная цитата при загрузке формы
+            Random rndT = new Random();
+            int txt = rndT.Next(1, 12);
+            switch (txt)
+            {
+                case 1: label2.Text = "Адольф Гитлер:"; label3.Text = "«Мы не имели ни малейшего понятия о том, насколько гигантской была подготовка противника!»"; break;
+                case 2: label2.Text = "Адольф Гитлер:"; label3.Text = "«Великие лжецы - это и великие волшебники.»"; break;
+                case 3: label2.Text = "Владимир Ленин:"; label3.Text = "«Первая мировая война - это лучший подарок революции.»"; break;
+                case 4: label2.Text = "Уинстон Черчилль:"; label3.Text = "«Англии был предложен выбор между войной и бесчестием. Она выбрала бесчестие и получит войну.»"; break;
+                case 5: label2.Text = "Иосиф Сталин:"; label3.Text = "«Отстаивать каждую пядь Советской земли, драться до последней капли крови!»"; break;
+                case 6: label2.Text = "Бенито Муссолини:"; label3.Text = "«Унизительно сидеть сложа руки, когда другие пишут историю. И неважно, кто победит.»"; break;
+                case 7: label2.Text = "Французский антивоенный лозунг:"; label3.Text = "«Зачем умирать за Данциг?»"; break;
+                case 8: label2.Text = "Альберт Эйнштейн:"; label3.Text = "«Пока существуют суверенные государства, обладающие мощными армиями, война неизбежна.»"; break;
+                case 9: label2.Text = "Уинстон Черчилль:"; label3.Text = "«Успех - это умение двигаться от одной неудачи к другой, не теряя энтузиазма.»"; break;
+                case 10: label2.Text = "Эрвин Роммель:"; label3.Text = "«В схватке один на один побеждает тот, у кого на один патрон больше.»"; break;
+                case 11: label2.Text = "Невилл Чемберлен:"; label3.Text = "«Я уверен, что мы обеспечили прочный мир.»"; break;
+                case 12: label2.Text = "Георгий Жуков:"; label3.Text = "«Подходя к минному полю, наша пехота проводит атаку так, как будто этого поля нет.»"; break;
+            }
+        }
+        Random rnd = new Random();
+        //Переменные, созданные для продвижения прогресс бара
+        int a;
+        int i;
+        int b;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Код отвечающий за прогресс бар
+            b = progressBar1.Value;
+            i++;
+            if (i < 4)
+            {
+                a = rnd.Next(1, 20);
+                progressBar1.Value += a;
+            }
+            else
+                if (i == 4) 
+                {
+                progressBar1.Value += 100 - b;
+                }
+        }
+    }
+}
